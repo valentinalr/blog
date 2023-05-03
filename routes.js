5b79145a5adcfcc40bc3ed3d7d9eb8c9f677cd79
Mateo Gómez <mateogomez1718@gmail.com>
2023-05-03 16:33:34 -0300

    Update routes.js

--- routes.js
+++ routes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mainController = require("./controllers/mainControllers");
 const router = express.Router();
 
-router.get("/", mainController.findAll);
+router.get("/home", mainController.findAll);
 router.get("/notice", mainController.findOne); //agregar :id despues
 router.get("/admin", mainController.admin);
 
