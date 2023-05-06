40832f5bf2e4556784af1ad99b64a6c53a7df141
Mateo Gómez <mateogomez1718@gmail.com>
2023-05-06 16:33:52 -0300

    added .env and changed the connection settings

--- models/index.js
+++ models/index.js
@@ -1,10 +1,17 @@
+require("dotenv").config();
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize("blog_database", "root", "root", {
-  host: "127.0.0.1",
-  port: 3306,
-  dialect: "mysql",
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USERNAME,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: process.env.DB_DIALECT,
+    logging: false,
+  }
+);
 
 const Article = require("./Article");
 const Author = require("./Author");
