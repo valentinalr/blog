8c96d7538016cbf4216e0f1a6924c99164a0f779
ffierro4 <facundofierro49@gmail.com>
2023-05-08 14:07:38 -0300

    Update routes

--- routes.js
+++ routes.js
@@ -2,16 +2,23 @@ const express = require("express");
 const articleController = require("./controllers/articleController");
 const router = express.Router();
 
+//Retorna los articulos en formato JSON.
 router.get("/api/articulos", articleController.apiArticle);
+
+//Muestra todos los articulos, o individualmente de acuerdo a su id.
 router.get("/home", articleController.findAllArticle);
 router.get("/article/:id", articleController.findOneArticle);
 
+//Vista de la pagina administrador, donde se da la opción de crear, modificar y eliminar un articulo.
+router.get("/admin", articleController.admin);
+
+//Rutas y vistas de opciones "editar" articulo.
 router.post("/admin/crear", articleController.storeArticle);
 router.post("/article/:id", articleController.storeEdit);
-router.get("/article/delete/:id", articleController.destroyArticle);
-
-router.get("/admin", articleController.admin);
 router.get("/admin/crear", articleController.formUploadArticle);
 router.get("/admin/editar/:id", articleController.formUpdateArticle);
+
+//Funcionalidad del botón "eliminar". (No requiere una vista)
+router.get("/article/delete/:id", articleController.destroyArticle);
 
 module.exports = router;
