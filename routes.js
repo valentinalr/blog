cad108e06bbe108e119416ec58d4a098f82898ac
Ivanna Sugliano <126933494+Noah1711@users.noreply.github.com>
2023-05-09 17:02:21 -0300

    routesRegister

--- routes.js
+++ routes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const articleController = require("./controllers/articleController");
 const commentController = require("./controllers/commentController");
+// const registerController = require("./controllers/registerController");
 const router = express.Router();
 
 //Retorna los articulos en formato JSON.
@@ -24,5 +25,14 @@ router.post("/article/review/:id", commentController.review);
 
 //Funcionalidad del botón "eliminar". (No requiere una vista)
 router.get("/article/delete/:id", articleController.destroyArticle);
+
+// //Rutas del Registro
+// router.get("/registro", registerController.viewRegister);
+// router.post("/registro", registerController.createUser);
+
+// //Rutas del Login/logout
+// router.get("/login", registerController.viewLogin);
+// router.post("/login", registerController.loginUser);
+// router.get("/logout", registerController.logoutUser);
 
 module.exports = router;
