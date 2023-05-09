5491ba2f41948d80435fcb890030ebc5b5ca1fad
Pataty22 <110777470+Pataty22@users.noreply.github.com>
2023-05-09 16:11:30 -0300

    seeders

--- models/index.js
+++ models/index.js
@@ -1,16 +1,14 @@
-require("dotenv").config();
 const { Sequelize } = require("sequelize");
 const articleSeeder = require("../seeders/articleSeeder");
 
 const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USERNAME,
-  process.env.DB_PASSWORD,
+  process.env.DB_DATABASE, // Ej: hack_academy_db
+  process.env.DB_USERNAME, // Ej: root
+  process.env.DB_PASSWORD, // Ej: root
   {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: process.env.DB_DIALECT,
-    logging: false,
+    host: process.env.DB_HOST, // Ej: 127.0.0.1
+    dialect: process.env.DB_CONNECTION, // Ej: mysql
+    logging: false, // Para que no aparezcan mensajes en consola.
   }
 );
 
@@ -22,17 +20,19 @@ Article.initModel(sequelize);
 Author.initModel(sequelize);
 Comment.initModel(sequelize);
 
-(async function () {
-  await articleSeeder(Article);
-})();
-
 Author.hasMany(Article);
 Article.belongsTo(Author);
 
 Article.hasMany(Comment);
 Comment.belongsTo(Article);
 
-sequelize.sync({ alter: true });
+sequelize.sync({ force: true }).then(async () => {
+  await articleSeeder(Article);
+});
+
+//(async function () {
+//  await articleSeeder(Article);
+//})();
 
 module.exports = {
   sequelize,
