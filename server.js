2464e586bf7f961e8822e74fd259117f0f1a2870
Pataty22 <110777470+Pataty22@users.noreply.github.com>
2023-05-10 12:24:21 -0300

    cambio en nav- add logout

--- server.js
+++ server.js
@@ -1,14 +1,21 @@
 const express = require("express");
 const app = express();
 const routes = require("./routes");
-// const session = require("express-session");
-// const passport = require("passport");
-// const localStrategy = require("passport-local");
+const session = require("express-session");
+const passport = require("passport");
+const localStrategy = require("passport-local");
 
 app.set("view engine", "ejs");
 
 //Middlewares
-app.use(session({ secret: "AlgúnTextoSuperSecreto", resave: false }));
+app.use(
+  session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
 app.use(passport.session());
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
