7688bfb31fb4dbd6e93034942a5bc754d678e4e7
valentinalr <valentina.lerose@outlook.com>
2023-05-10 12:09:59 -0300

    login update

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -1,6 +1,6 @@
-// const session = require("express-session");
-// const passport = require("passport");
-// const localStrategy = require("passport-local");
+const session = require("express-session");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
 const bcrypt = require("bcryptjs");
 const { Author } = require("../models");
 
@@ -8,27 +8,77 @@ function viewLogin(req, res) {
   res.render("login");
 }
 
-async function loginUser(req, res) {
-  const author = await Author.findAll({ where: { email: req.body.email } });
-  const pass = req.body.password;
-  const savedUser = author[0].password;
+function passportConfig() {
+  passport.use(
+    new LocalStrategy(
+      { usernameField: "email" },
+      async (email, password, cb) => {
+        const user = await Author.findOne({ where: { email: email } });
 
-  const passCheck = await bcrypt.compare(pass, savedUser);
+        if (!user) {
+          return done(null, false, { message: "Credenciales incorrectas" });
+        }
+        const checkPassword = await bcrypt.compare(password, user.password);
+        if (!checkPassword) {
+          return done(null, false, { message: "Credenciales incorrectas" });
+        }
+        return done(null, user);
+      }
+    )
+  );
 
-  if (passCheck) {
-    return res.redirect("home");
-  } else {
-    const errorMessage = "Invalid email or password";
-    return res.render("login", { error: errorMessage });
-  }
-  //   passport.use(
-  //     localStrategy((email, password, cb) => {
-  //       //details go here
-  //     })
-  //);
+  passport.serializeUser(function (user, done) {
+    done(null, user.id);
+  });
+  passport.deserializeUser(async function (id, done) {
+    try {
+      const user = await Author.findByPk(id);
+      done(null, user);
+    } catch (error) {
+      done(error);
+    }
+  });
 }
+
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  } else {
+    req.session.redirectTo = req.query.redirectTo;
+    res.redirect("/login");
+  }
+}
+
+function login(req, res) {
+  passport.authenticate("local", {
+    successRedirect: "/home",
+    failureRedirect: "/login",
+  })(req, res);
+}
+
+// async function loginUser(req, res) {
+//   const author = await Author.findOne({ where: { email: req.body.email } });
+//   const pass = req.body.password;
+//   const savedUser = author[0].password;
+
+//   const passCheck = await bcrypt.compare(pass, savedUser);
+
+//   if (passCheck) {
+//     return res.redirect("home");
+//   } else {
+//     const errorMessage = "Invalid email or password";
+//     return res.render("login", { error: errorMessage });
+//   }
+//   //   passport.use(
+//   //     localStrategy((email, password, cb) => {
+//   //       //details go here
+//   //     })
+//   //);
+// }
 
 module.exports = {
   viewLogin,
-  loginUser,
+  passportConfig,
+  login,
+  ensureAuthenticated,
 };
--- server.js
+++ server.js
@@ -1,19 +1,28 @@
 const express = require("express");
 const app = express();
 const routes = require("./routes");
-// const session = require("express-session");
-// const passport = require("passport");
-// const localStrategy = require("passport-local");
+const session = require("express-session");
+const passport = require("passport");
+const { passportConfig } = require("./controllers/loginController");
 
 app.set("view engine", "ejs");
 
 //Middlewares
-app.use(session({ secret: "AlgúnTextoSuperSecreto", resave: false }));
-app.use(passport.session());
-app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
 
+app.use(
+  session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+app.use(passport.session());
+passportConfig();
+
+app.use(express.static("public"));
+app.use(express.json());
 app.use(routes);
 
 //Server listening
