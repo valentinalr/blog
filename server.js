e6ae09bfed5da047339e7f57196ead78ee975296
Pataty22 <110777470+Pataty22@users.noreply.github.com>
2023-05-10 12:46:47 -0300

    .

--- server.js
+++ server.js
@@ -3,16 +3,12 @@ const app = express();
 const routes = require("./routes");
 const session = require("express-session");
 const passport = require("passport");
-<<<<<<< HEAD
 const localStrategy = require("passport-local");
-=======
 const { passportConfig } = require("./controllers/loginController");
->>>>>>> f98fdcb39b7d8956b89c17e517524828b106a869
 
 app.set("view engine", "ejs");
 
 //Middlewares
-<<<<<<< HEAD
 app.use(
   session({
     secret: process.env.SECRET,
@@ -23,8 +19,6 @@ app.use(
 
 app.use(passport.session());
 app.use(express.static("public"));
-=======
->>>>>>> f98fdcb39b7d8956b89c17e517524828b106a869
 app.use(express.urlencoded({ extended: true }));
 
 app.use(
