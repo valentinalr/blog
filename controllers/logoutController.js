03c2d6e10c55dd41e80e3d3e5a2591ca472b839e
Pataty22 <110777470+Pataty22@users.noreply.github.com>
2023-05-11 09:08:02 -0300

    .

--- controllers/logoutController.js
+++ controllers/logoutController.js
@@ -1,7 +1,6 @@
 const session = require("express-session");
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
-const bcrypt = require("bcryptjs");
 const { Author } = require("../models");
 
 async function logoutUser(req, res) {
